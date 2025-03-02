3420035f39e74038df67ad4c743d282db81c7acc
Afrin Fathima I <afrin.shah2004@gmail.com>
2025-03-02 18:03:33 +0530

    Add files via upload

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layouts/Layouts'
+
+const HomePage = () => {
+  return (
+    <Layout>
+        <h1>
+            Home Page
+        </h1>
+    </Layout>
+  );
+};
+
+export default HomePage
